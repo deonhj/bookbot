604d3ed5bd957dea27e0dff0f7aa361e698109f7
deonhj <deonhjordaan@gmail.com>
2025-04-17 21:35:27 +0200

    add report

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+from stats import get_num_words, get_num_chars, get_count_per_char
+
 def main():
     file_content = get_book_text('books/frankenstein.txt')
-    print(f"{get_word_count(file_content)} words found in the document.")
+    print(f"{get_num_words(file_content)} words found in the document.")
+    print(f"{get_num_chars(file_content)} characters found in the document.")
+    print(get_count_per_char(file_content.lower()))
 
 
 def get_book_text(file_path):
--- stats.py
+++ stats.py
@@ -1,7 +1,30 @@
-def get_word_count(file_content):
+def get_num_words(file_content):
     """
     Counts the number of words in the given text.
     """
 
     words = file_content.split()
     return len(words)
+
+def get_num_chars(file_content):
+    """
+    Counts the number of characters in the given text.
+    """
+
+    return len(file_content)
+
+
+def get_count_per_char(file_content):
+    """
+    Counts the number of occurrences of each character in the given text.
+    """
+
+    count = {}
+    for char in file_content.lower():
+        if char in count:
+            count[char] += 1
+        else:
+            count[char] = 1
+    return count
+
+
